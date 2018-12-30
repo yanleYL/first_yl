86fdfd3f9ceb009ee4dea6d42e0ea396d553e6c1
yanle <852270816@qq.com>
2018-12-30 10:01:18 +0800

    新建的类

---
+++ maven_web/src/main/java/com/itheima/controller/LeftController.java
@@ -0,0 +1,12 @@
+package com.itheima.controller;
+
+/**
+ * Project:maven_Test
+ * Package: com.itheima.controller
+ * Author: yanle
+ * Date: 2018/12/30
+ * Version: 1.0
+ * Time:10:00
+ */
+public class LeftController {
+}
